852c3ed6bde8e0b5d7920f3bf850983f71ee9ec8
nxl <ningwenchao@163.com>
2017-12-15 15:26:44 +0800

    fix:base packages load strategy
    the priority:
    1)configured base packages(spring.cloud.grpc.client.package-to-scan) first
    2)then will be the basePackages attribute of the @EnableGrpcClients Annotation
    3)if both are missing,we will use the root package,ana,the package of the main class(Application)

--- spring-cloud-starter-grpc-client/src/main/java/com/icekredit/rpc/grpc/client/GrpcClientRegistrar.java
+++ spring-cloud-starter-grpc-client/src/main/java/com/icekredit/rpc/grpc/client/GrpcClientRegistrar.java
@@ -1,11 +1,11 @@
 package com.icekredit.rpc.grpc.client;
 
+import com.icekredit.rpc.grpc.client.annotation.EnableGrpcClients;
 import com.icekredit.rpc.grpc.client.scanner.GrpcClientBeanScanner;
-import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.context.EnvironmentAware;
@@ -16,26 +16,21 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.core.type.StandardAnnotationMetadata;
 import org.springframework.util.ClassUtils;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class GrpcClientRegistrar implements ImportBeanDefinitionRegistrar,
-        ResourceLoaderAware, BeanClassLoaderAware, EnvironmentAware {
+        ResourceLoaderAware, EnvironmentAware {
     private ResourceLoader resourceLoader;
-
-    private ClassLoader beanClassLoader;
 
     private Environment environment;
 
     private static final String SPRING_GRPC_CLIENT_PACKAGE_TO_SCAN = "spring.cloud.grpc.client.package-to-scan";
 
     private Logger logger = LoggerFactory.getLogger(GrpcClientRegistrar.class);
-
-    @Override
-    public void setBeanClassLoader(ClassLoader classLoader) {
-        this.beanClassLoader = classLoader;
-    }
 
     @Override
     public void setEnvironment(Environment environment) {
@@ -49,63 +44,80 @@ public class GrpcClientRegistrar implements ImportBeanDefinitionRegistrar,
 
     @Override
     public void registerBeanDefinitions(AnnotationMetadata metadata, BeanDefinitionRegistry beanDefinitionRegistry) {
+        if (!(metadata instanceof StandardAnnotationMetadata)) {
+            return;
+        }
+
         GrpcClientBeanScanner scanner = new GrpcClientBeanScanner(beanDefinitionRegistry);
         scanner.setResourceLoader(resourceLoader);
         scanner.setBeanNameGenerator(new AnnotationBeanNameGenerator());
         scanner.setScopedProxyMode(ScopedProxyMode.INTERFACES);
 
-        RelaxedPropertyResolver resolver = new RelaxedPropertyResolver(environment);
-        setScannedPackages(scanner, resolver.getProperty(SPRING_GRPC_CLIENT_PACKAGE_TO_SCAN));
-    }
+        String[] configuredBasePackages = getConfiguredBasePackages();
+        String[] annotatedBasePackages = getAnnotatedBasePackages((StandardAnnotationMetadata) metadata);
+        String rootPackage = getRootPackage((StandardAnnotationMetadata) metadata);
 
-    private void setScannedPackages(GrpcClientBeanScanner beanScanner, String basePackages) {
-        if (StringUtils.isBlank(basePackages)) {
-            logger.error("the package to scan has not been specified!");
-            return;
-        }
+        //如果配置文件中指定了basePackages，那么直接扫描配置文件中指定的basePackages
+        //否则扫描注解中指定的basePackages，
+        //如果注解中仍然没有指定basePackages，那么扫描主类所在根包package
+        if (ArrayUtils.isEmpty(configuredBasePackages)
+                && ArrayUtils.isEmpty(annotatedBasePackages)){
+            logger.info("No base packages specified for grpc client!We will scan the root packages:{}",rootPackage);
 
-        int delimiterIndex = StringUtils.indexOf(basePackages, ",");
-        if (delimiterIndex > -1) {
-            StringTokenizer tokenizer = new StringTokenizer(basePackages, ",");
-            Set<String> packageToScanSet = new HashSet<>();
-            while (tokenizer.hasMoreTokens()) {
-                String subPackage = tokenizer.nextToken();
-                packageToScanSet.add(subPackage);
-                logger.info("Subpackage {} is to be scanned by {}", subPackage, beanScanner);
-            }
+            scanner.scan(rootPackage);
+        } else if (ArrayUtils.isNotEmpty(configuredBasePackages)){
+            logger.info("Configured base packages founded:{}",Arrays.toString(configuredBasePackages));
 
-            List<String> packageToScanList = new ArrayList<>(packageToScanSet);
-            String[] packagesToScan = packageToScanList.toArray(new String[packageToScanList.size()]);
-            beanScanner.scan(packagesToScan);
+            scanner.scan(configuredBasePackages);
         } else {
-            logger.info("Base package {} is to be scanned with {}", basePackages, beanScanner);
-            beanScanner.scan(basePackages);
+            logger.info("Annotated base packages founded:{}",Arrays.toString(annotatedBasePackages));
+
+            scanner.scan(annotatedBasePackages);
         }
     }
 
-    private Set<String> getBasePackages(Map<String, Object> annotationAttributes, Class<? extends AnnotationMetadata> annotationMetadataClass) {
-        Set<String> basePackages = new HashSet<>();
-
-        if (MapUtils.isEmpty(annotationAttributes)) {
-            return basePackages;
+    /**
+     * 获取由配置文件指定的basePackages，由spring.cloud.grpc.client.package-to-scan指定，并使用`,`进行分隔
+     *
+     * @return 配置文件中指定的basePackages
+     */
+    private String[] getConfiguredBasePackages() {
+        RelaxedPropertyResolver resolver = new RelaxedPropertyResolver(environment);
+        String basePackagesStr = resolver.getProperty(SPRING_GRPC_CLIENT_PACKAGE_TO_SCAN);
+        if (StringUtils.isBlank(basePackagesStr)) {
+            return new String[0];
         }
 
-        for (String basePackage : (String[]) annotationAttributes.get("values")) {
-            if (StringUtils.isNotBlank(basePackage)) {
-                basePackages.add(basePackage);
-            }
-        }
+        String[] basePackages = basePackagesStr.split(",");
 
-        for (String basePackage : (String[]) annotationAttributes.get("basePackages")) {
-            if (StringUtils.isNotBlank(basePackage)) {
-                basePackages.add(basePackage);
-            }
-        }
+        return (String[]) Arrays.stream(basePackages)
+                .filter(StringUtils::isNotBlank)
+                .map(String::trim)
+                .distinct()
+                .collect(Collectors.toSet()).toArray();
+    }
 
-        if (basePackages.isEmpty()) {
-            basePackages.add(ClassUtils.getPackageName(annotationMetadataClass));
+    /**
+     * 根据主类上面的EnableGrpcClients注解获取需要扫描的包名
+     *
+     * @param standardAnnotationMetadata 主类上面的注解信息元数据
+     * @return 注解指定的basePackages
+     */
+    private String[] getAnnotatedBasePackages(StandardAnnotationMetadata standardAnnotationMetadata) {
+        String[] basePackages = null;
+
+        if (standardAnnotationMetadata.hasAnnotation(EnableGrpcClients.class.getName())) {
+            Map<String, Object> attrs = standardAnnotationMetadata
+                    .getAnnotationAttributes(EnableGrpcClients.class.getName());
+
+            basePackages = (String[]) attrs.get("values");
+            basePackages = basePackages.length == 0 ? (String[]) attrs.get("basePackages") : basePackages;
         }
 
         return basePackages;
+    }
+
+    private String getRootPackage(StandardAnnotationMetadata standardAnnotationMetadata){
+        return ClassUtils.getPackageName(standardAnnotationMetadata.getIntrospectedClass());
     }
 }
